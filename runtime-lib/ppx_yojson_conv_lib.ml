3c3c6ee6fa90307eed7d072b42022276ff27c248
Xavier Clerc <xclerc@janestreet.com>
2019-09-04 09:33:50 +0100

    v0.13-preview.121.02+24

--- runtime-lib/ppx_yojson_conv_lib.ml
+++
@@ -1,9 +0,0 @@
-module Yojson_conv_error = Yojson_conv_error
-module Yojson_conv = Yojson_conv
-module Yojsonable = Yojsonable_intf
-module Yojson = Yojson
-
-external ignore : _ -> unit = "%ignore"
-external poly_equal : 'a -> 'a -> bool = "%equal"
-
-let ( ! ) : 'a ref -> 'a = fun x -> !x
