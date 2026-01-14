bf95e7cf422a8f10ca7c38bfe41c47783b2dc462
public-release <as-public-release@janestreet.com>
2026-01-14 01:07:05 +0000

    v0.18~preview.130.83+317

--- test/ppx_yojson_test.ml
+++ test/ppx_yojson_test.ml
@@ -939,7 +939,7 @@ module Drop_if = struct
          [@yojson_drop_if
            fun x ->
              (* pa_type_conv used to drop parens altogether, causing type errors in the
-                 following code *)
+                following code *)
              let pair = x, 2 in
              match Some pair with
              | None -> true
@@ -1330,7 +1330,8 @@ module Variance = struct
 end
 
 module Clash = struct
-  (* Same name for type-var and type-name; must be careful when introducing rigid type names. *)
+  (* Same name for type-var and type-name; must be careful when introducing rigid type
+     names. *)
   type 'hey hey = Hey of 'hey [@@deriving yojson]
   type 'hey rigid_hey = Hey of 'hey [@@deriving yojson]
   type ('foo, 'rigid_foo) foo = Foo of 'foo [@@deriving yojson]
